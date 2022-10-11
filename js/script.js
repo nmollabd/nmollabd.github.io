c6b42870f6aebb4770e747e8b2909a37d7639f1d
Naim Molla <96284855+nmollabd@users.noreply.github.com>
2022-10-11 19:38:00 +0600

    Popup Video update

--- js/script.js
+++ js/script.js
@@ -240,21 +240,21 @@
     });
 
     // myCursor    
-    var myCursor    = jQuery('.mouse-cursor');
+    // var myCursor    = jQuery('.mouse-cursor');
     
-        if(myCursor.length){
-            if ($("body")) {
-            const e = document.querySelector(".cursor-inner"),
-                t = document.querySelector(".cursor-outer");
-            let n, i = 0,
-                o = !1;
-            window.onmousemove = function (s) {
-                o || (t.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)"), e.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)", n = s.clientY, i = s.clientX
-            }, $("body").on("mouseenter", "a, .ryker_tm_topbar .trigger, .cursor-pointer", function () {
-                e.classList.add("cursor-hover"), t.classList.add("cursor-hover")
-            }), $("body").on("mouseleave", "a, .ryker_tm_topbar .trigger, .cursor-pointer", function () {
-                $(this).is("a") && $(this).closest(".cursor-pointer").length || (e.classList.remove("cursor-hover"), t.classList.remove("cursor-hover"))
-            }), e.style.visibility = "visible", t.style.visibility = "visible"
-        }
-    }
+    //     if(myCursor.length){
+    //         if ($("body")) {
+    //         const e = document.querySelector(".cursor-inner"),
+    //             t = document.querySelector(".cursor-outer");
+    //         let n, i = 0,
+    //             o = !1;
+    //         window.onmousemove = function (s) {
+    //             o || (t.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)"), e.style.transform = "translate(" + s.clientX + "px, " + s.clientY + "px)", n = s.clientY, i = s.clientX
+    //         }, $("body").on("mouseenter", "a, .ryker_tm_topbar .trigger, .cursor-pointer", function () {
+    //             e.classList.add("cursor-hover"), t.classList.add("cursor-hover")
+    //         }), $("body").on("mouseleave", "a, .ryker_tm_topbar .trigger, .cursor-pointer", function () {
+    //             $(this).is("a") && $(this).closest(".cursor-pointer").length || (e.classList.remove("cursor-hover"), t.classList.remove("cursor-hover"))
+    //         }), e.style.visibility = "visible", t.style.visibility = "visible"
+    //     }
+    // }
 }(jQuery));
