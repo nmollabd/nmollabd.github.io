0959e090b98541bfab8baf8ff44d609d46a5977a
Naim Molla <96284855+nmollabd@users.noreply.github.com>
2022-11-04 15:00:46 +0600

    Protfolio Home Page Update

--- js/script.js
+++ js/script.js
@@ -295,16 +295,12 @@
 
     // WOW JS
     jQuery(window).on('load', function() {
-
         // WOW JS
         new WOW().init();
 
         // Preloader
         var preLoder = $("#preloader");
         preLoder.fadeOut(0);
-
-        //ProgressBar 
-        // jQuery(".progress-bar").ProgressBar(); 
     });
 
     // myCursor    
@@ -326,24 +322,6 @@
     //     }
     // }
 }(jQuery));
-
-// Clock
-const deg = 6;
-const hour = document.querySelector(".hour");
-const min = document.querySelector(".min");
-const sec = document.querySelector(".sec");
-const setClock = () => {
-    let day = new Date();
-    let hh = day.getHours() * 30;
-    let mm = day.getMinutes() * deg;
-    let ss = day.getSeconds() * deg;
-
-    hour.style.transform = `rotateZ(${hh + mm / 12}deg)`;
-    min.style.transform = `rotateZ(${mm}deg)`;
-    sec.style.transform = `rotateZ(${ss}deg)`;
-};
-setClock();
-setInterval(setClock, 1000);
 
 // DOMContentLoaded
 window.addEventListener("resize", resizeCanvas, false);
