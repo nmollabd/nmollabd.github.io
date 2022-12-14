9a3a31a682ed979a365d748fe787be7090bedb11
Naim Molla <96284855+nmollabd@users.noreply.github.com>
2022-12-14 15:32:50 +0600

    Protfolio contact page Update

--- js/script.js
+++ js/script.js
@@ -414,25 +414,38 @@
         direction: "next",
     });
 
-    // Disable right-click
-    document.addEventListener('contextmenu', (e) => e.preventDefault());
-    function ctrlShiftKey(e, keyCode) {
-        return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
-    }
+    var bars = document.querySelectorAll('.progressbar-wrap > .progressbar');
+    console.clear();
 
-    document.onkeydown = (e) => {
-        // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+    setInterval(function(){
+      bars.forEach(function(bar){
+        var getWidth = parseFloat(bar.dataset.progress);
+        
+        for(var i = 0; i < getWidth; i++) {
+          bar.style.width = i + '%';
+        }
+      });
+    }, 500);
 
-        if (
-            event.keyCode === 123 ||
-            ctrlShiftKey(e, 'I') ||
-            ctrlShiftKey(e, 'J') ||
-            ctrlShiftKey(e, 'C') ||
-            (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
-        )
+    // // Disable right-click
+    // document.addEventListener('contextmenu', (e) => e.preventDefault());
+    // function ctrlShiftKey(e, keyCode) {
+    //     return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+    // }
 
-        return false;
-    };
+    // document.onkeydown = (e) => {
+    //     // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+
+    //     if (
+    //         event.keyCode === 123 ||
+    //         ctrlShiftKey(e, 'I') ||
+    //         ctrlShiftKey(e, 'J') ||
+    //         ctrlShiftKey(e, 'C') ||
+    //         (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+    //     )
+
+    //     return false;
+    // };
 }(jQuery));
 
 $(document).ready(function() {
@@ -454,3 +467,110 @@ $(document).ready(function() {
         $('.overlay, body').addClass('loaded');
     }, 60000);
 })
+
+// ParticlesJS Config.
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 80,
+      "density": {
+        "enable": true,
+        "value_area": 700
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "grab"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 140,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
