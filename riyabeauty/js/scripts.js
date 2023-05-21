d37ccbece6b98e1eae0cf265523925b414f243d3
Naim Molla <96284855+nmollabd@users.noreply.github.com>
2023-05-21 16:45:07 +0600

    new riyabeauty desing

--- riyabeauty/js/scripts.js
+++ riyabeauty/js/scripts.js
@@ -582,7 +582,7 @@
 
     wowjs.init();
 
-    Disable right-click
+    // Disable right-click
     document.addEventListener('contextmenu', (e) => e.preventDefault());
     function ctrlShiftKey(e, keyCode) {
         return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
