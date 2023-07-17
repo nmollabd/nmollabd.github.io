c2aeb9de8831bbdeec6e7a1c1f078976d3bb2526
Naim Molla <96284855+nmollabd@users.noreply.github.com>
2023-07-17 16:46:38 +0600

    new riyabeauty landing page desing update

--- portfolioupdated/js/scripts.js
+++ portfolioupdated/js/scripts.js
@@ -635,24 +635,24 @@
 	document.body.classList.add( BrowserDetect.browser );
 
     // Disable right-click
-    document.addEventListener('contextmenu', (e) => e.preventDefault());
-    function ctrlShiftKey(e, keyCode) {
-        return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
-    }
+    // document.addEventListener('contextmenu', (e) => e.preventDefault());
+    // function ctrlShiftKey(e, keyCode) {
+    //     return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+    // }
 
-    document.onkeydown = (e) => {
-        // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+    // document.onkeydown = (e) => {
+    //     // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
 
-        if (
-            event.keyCode === 123 ||
-            ctrlShiftKey(e, 'I') ||
-            ctrlShiftKey(e, 'J') ||
-            ctrlShiftKey(e, 'C') ||
-            (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
-        )
+    //     if (
+    //         event.keyCode === 123 ||
+    //         ctrlShiftKey(e, 'I') ||
+    //         ctrlShiftKey(e, 'J') ||
+    //         ctrlShiftKey(e, 'C') ||
+    //         (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+    //     )
 
-        return false;
-    };
+    //     return false;
+    // };
 
 	/*** Cursor */
 	const cursor = document.querySelector('#cursor');
